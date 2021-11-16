5f7869f0aed2c253abff2db2e821fbc16c83ec6f
Mike Degatano <michael.degatano@gmail.com>
2021-11-16 10:38:21 -0500

    Initial state of the addon

---
+++ amridm2mqtt/rootfs/amridm2mqtt/amridm2mqtt.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+"""
+Runs rtlamr to watch for IDM broadcasts from power meter. If meter id
+is in the list, usage is sent to 'readings/{meter id}/meter_reading'
+topic on the MQTT broker specified in settings.
+
+WATCHED_METERS = A Python list indicating those meter IDs to record and post.
+MQTT_HOST = String containing the MQTT server address.
+MQTT_PORT = An int containing the port the MQTT server is active on.
+
+"""
+import logging
+import subprocess
+import signal
+import sys
+import time
+import paho.mqtt.publish as publish
+import settings
+
+
+def shutdown():
+    """Uses signal to shutdown and hard kill opened processes and self."""
+    rtltcp.send_signal(15)
+    rtlamr.send_signal(15)
+    time.sleep(1)
+    rtltcp.send_signal(9)
+    rtlamr.send_signal(9)
+    sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, shutdown)
+signal.signal(signal.SIGINT, shutdown)
+
+# stores last interval id to avoid duplication, includes getter and setter
+last_reading = {}
+
+if len(settings.MQTT_USER) and len(settings.MQTT_PASSWORD):
+    AUTH = {"username": settings.MQTT_USER, "password": settings.MQTT_PASSWORD}
+else:
+    AUTH = None
+
+logging.basicConfig()
+logging.getLogger().setLevel(settings.LOG_LEVEL)
+
+
+def get_last_interval(meter_id):  # pylint: disable=redefined-outer-name
+    """Get last interval."""
+    return last_reading.get(meter_id, (None))
+
+
+def set_last_interval(meter_id, interval_id):  # pylint: disable=redefined-outer-name
+    """Set last interval."""
+    last_reading[meter_id] = interval_id
+
+
+def send_mqtt(
+    topic,
+    payload,
+):
+    """Send data to MQTT broker defined in settings."""
+    try:
+        publish.single(
+            topic,
+            payload=payload,
+            qos=1,
+            hostname=settings.MQTT_HOST,
+            port=settings.MQTT_PORT,
+            auth=AUTH,
+            tls=settings.MQTT_TLS,
+        )
+    except Exception as ex:  # pylint: disable=broad-except
+        logging.error("MQTT Publish Failed: %s", str(ex))
+
+
+# start the rtl_tcp program
+rtltcp = subprocess.Popen(
+    [settings.RTL_TCP + " > /dev/null 2>&1 &"],
+    shell=True,
+    stdin=None,
+    stdout=None,
+    stderr=None,
+    close_fds=True,
+)
+time.sleep(5)
+
+# start the rtlamr program.
+rtlamr_cmd = [settings.RTLAMR, "-msgtype=idm", "-format=csv"]
+rtlamr = subprocess.Popen(
+    rtlamr_cmd,
+    stdout=subprocess.PIPE,
+    universal_newlines=True,
+)
+
+while True:
+    try:
+        amrline = rtlamr.stdout.readline().strip()
+        flds = amrline.split(",")
+
+        if len(flds) != 66:
+            # proper IDM results have 66 fields
+            continue
+
+        # make sure the meter id is one we want
+        meter_id = int(flds[9])
+        if settings.WATCHED_METERS and meter_id not in settings.WATCHED_METERS:
+            continue
+
+        # get some required info: current meter reading,
+        # current interval id, most recent interval usage
+        read_cur = int(flds[15])
+        interval_cur = int(flds[10])
+        idm_read_cur = int(flds[16])
+
+        # retreive the interval id of the last time we sent to MQTT
+        interval_last = get_last_interval(meter_id)
+
+        if interval_cur != interval_last:
+
+            # as observed on on my meter...
+            # using values set in settings...
+            # each idm interval is 5 minutes (12x per hour),
+            # measured in hundredths of a kilowatt hour
+            # take the last interval usage times 10 to get watt-hours,
+            # then times 12 to get average usage in watts
+            rate = idm_read_cur * settings.WH_MULTIPLIER * settings.READINGS_PER_HOUR
+
+            current_reading_in_kwh = (read_cur * settings.WH_MULTIPLIER) / 1000
+
+            logging.debug(
+                "Sending meter %s reading: %s", meter_id, current_reading_in_kwh
+            )
+            send_mqtt(
+                f"readings/${meter_id}/meter_reading",
+                str(current_reading_in_kwh),
+            )
+
+            logging.debug("Sending meter %s rate: %s", meter_id, rate)
+            send_mqtt(f"readings/${meter_id}/meter_rate", str(rate))
+
+            # store interval ID to avoid duplicating data
+            set_last_interval(meter_id, interval_cur)
+
+    except Exception as ex:  # pylint: disable=broad-except
+        logging.debug("Exception squashed! %s: %s", ex.__class__.__name__, ex)
+        time.sleep(2)
---
+++ amridm2mqtt/rootfs/amridm2mqtt/settings.py
@@ -0,0 +1,69 @@
+"""Get our settings from os.environ to facilitate running in Docker.
+"""
+import os
+import logging
+
+# List of the Meter IDs to watch
+# Use empty brackets to read all meters - []
+# List may contain only one entry - [12345678]
+# or multiple entries - [12345678, 98765432, 12340123]
+power_meters = os.environ['WATCHED_METERS'].replace(',', ' ').split(' ')
+WATCHED_METERS = [int(meter_id) for meter_id in power_meters]
+
+# multiplier to get reading to Watt Hours (Wh)
+# examples:
+#   for meter providing readings in kWh
+#      MULTIPLIER = 1000
+#   for meter providing readings in kWh
+#   with 2 extra digits of precision
+#      MULTIPLIER = 10
+# MULTIPLIER needs to be a number
+WH_MULTIPLIER = int(os.environ.get('WH_MULTIPLIER', 1000))
+
+# number of IDM intervals per hour reported by the meter
+# examples:
+#   for meter providing readings every 5 minutes
+#   or 12 times every hour
+#     READINGS_PER_HOUR = 12
+#   for meter providing readings every 15 minutes
+#   or 12 times every hour
+#     READINGS_PER_HOUR = 4
+READINGS_PER_HOUR = int(os.environ.get('READINGS_PER_HOUR', 12))
+
+# MQTT Server settings
+# MQTT_HOST needs to be a string
+# MQTT_PORT needs to be an int
+# MQTT_USER needs to be a string
+# MQTT_PASSWORD needs to be a string
+# If no authentication, leave MQTT_USER and MQTT_PASSWORD empty
+MQTT_HOST = os.environ.get('MQTT_HOST', '127.0.0.1')
+MQTT_PORT = int(os.environ.get('MQTT_PORT', 1883))
+MQTT_USER = os.environ.get('MQTT_USER', '')
+MQTT_PASSWORD = os.environ.get('MQTT_PASSWORD', '')
+
+# If TLS in use, a CA file must be provided
+# Client Key+Cert can optionally be provided as well
+if os.environ.get('MQTT_CA'):
+    MQTT_TLS = {
+        'ca_certs': os.environ.get('MQTT_CA'),
+        'certfile': os.environ.get('MQTT_CERT'),
+        'keyfile': os.environ.get('MQTT_KEY')
+    }
+else:
+    MQTT_TLS = None
+
+# Set logging level
+EV_TO_LOG_LEVEL = {
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARNING': logging.WARNING,
+    'ERROR': logging.ERROR,
+    'CRITICAL': logging.CRITICAL
+}
+LOG_LEVEL = EV_TO_LOG_LEVEL.get(os.environ.get('LOG_LEVEL'))
+
+# path to rtlamr
+RTLAMR = '/root/go/bin/rtlamr'
+
+# path to rtl_tcp
+RTL_TCP = '/usr/bin/rtl_tcp'

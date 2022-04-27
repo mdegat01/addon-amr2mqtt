251112985c2a7dbb634c35a84bed871abae3cb9b
Mike Degatano <michael.degatano@gmail.com>
2022-04-27 15:59:20 -0400

    Fix name for last seen sensor

--- amr2mqtt/rootfs/amr2mqtt/amr2mqtt.py
+++ amr2mqtt/rootfs/amr2mqtt/amr2mqtt.py
@@ -307,6 +307,7 @@ def send_discovery_messages():
                 category=CATEGORY_DIAGNOSTIC,
             )
             last_seen["device_class"] = "timestamp"
+            last_seen["name"] = "Last Seen"
             publish_sensor_discovery(
                 meter_id=meter_id,
                 device=device,
